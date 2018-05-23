c2b6cd4f7e85af960fa259357911d6217fa53176
Nathan Hill <nateohill@gmail.com>
2018-05-23 17:58:09 +0000

    automatically backed up by learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -1,4 +1,8 @@
 def turn_count(board)
+  x = 0
   board.each do | board |
+    x += 1
+    if board[x] === "" board[x] === " " board[x] === nil
+  end
   # for reference primary_colors.each do |color|
 end
